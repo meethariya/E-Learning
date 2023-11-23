70a8cf3e1347bacf7bc307d22ad13d970f4b3799
meethariya <meetkirtibhai@virtusa.com>
2023-11-23 11:46:49 +0530

    added angular

---
+++ E-Learning-Angular/E-Learning/src/app/modules/general/general.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GeneralRoutingModule } from './general-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    CommonModule,
+    GeneralRoutingModule
+  ]
+})
+export class GeneralModule { }
---
+++ E-Learning-Angular/E-Learning/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiGateway: 'http://localhost:8089',
+};
